2c5ebe283b04560376814054306bdb7a38830c83
Ruaridh Wilson <2146661w@student.gla.ac.uk>
2022-05-22 14:38:01 +0100

    add render method

--- buddha.py
+++ buddha.py
@@ -4,6 +4,7 @@ import numpy as np
 class Buddha:
     def __init__(self) -> None:
         self.paths = None
+        self.image = None
 
     def build(self, iterations: int = 100, n_points: int = 10_000) -> None:
         self.paths = np.zeros((iterations + 1, n_points), dtype=complex)
@@ -12,7 +13,20 @@ class Buddha:
         for i in range(1, iterations + 1):
             z = self.paths[i - 1, :] ** 2 + self.paths[0, :]  # z_{n+1} = z_n^2 + c
             self.paths[i, :] = np.where(np.abs(z) <= 2, z, np.nan)
-        return
 
-    def render(self, resolution: (int, int) = (1_000, 1_000), zoom: float = 3.5):
-        return
+    def render(self, resolution: (int, int) = (1_000, 1_000)) -> None:
+        if self.paths is None:
+            print("You must call Buddha.build() before rendering")
+            return
+
+        self.image = np.zeros(resolution, dtype=int)
+        escapees = np.isnan(self.paths[-1, :])
+        for step in self.paths[:, escapees]:
+            step = step[np.isfinite(step)]
+            step = (step + 2 + 2j) / 4 * max(resolution)
+            ys = step.imag.astype(int)
+            xs = step.real.astype(int)
+            ys = ys[ys < resolution[0]]
+            xs = xs[xs < resolution[1]]
+            self.image[ys, xs] += 1
+        self.image = 255 * self.image // self.image.max(initial=1)
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from buddha import Buddha
 
 if __name__ == '__main__':
     buddha = Buddha()
-    buddha.build(iterations=100, n_points=3)
-    print(buddha.paths)
+    buddha.build(iterations=100, n_points=1000)
+    buddha.render(resolution=(10, 10))
+    print(buddha.image)
